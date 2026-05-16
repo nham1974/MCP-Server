de7d473d906831f29170723e894b0de082da70e7
Rao <nham_pellur@live.com>
2026-05-16 16:10:48 -0400

    grret.py

---
+++ greet.py
@@ -0,0 +1,19 @@
+
+import os
+from fastmcp import FastMCP
+
+server = FastMCP("Sample-SSE-Server")
+
+@server.tool("greet")
+async def greet(params):
+    name = params.get("name", "Guest")
+    return f"Hello, {name}!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+
+    server.run(
+        transport="sse",
+        host="0.0.0.0",
+        port=port
+    )
